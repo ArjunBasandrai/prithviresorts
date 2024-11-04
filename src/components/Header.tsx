2ab477a8d0ca9e910454bcd22627e92cd9637e7a
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-11-05 02:32:30 +0530

    chore: added link to logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 import "../app/styles/globals.css";
 import Image from "next/image";
 import Navbar from "./Navbar";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <>
-        <header className="absolute px-6 lg:px-12 xl:px-20 py-6 w-full z-10 top-0 flex items-center justify-between">
-        <Image src="/logo.jpg" alt="Logo" width={150} height={100} />
-        <Navbar />
-        {/* <p>Contact</p> */}
-        </header>
+            <header className="absolute px-6 lg:px-12 xl:px-20 py-6 w-full z-10 top-0 flex items-center justify-between">
+                <Link href="/">
+                    <Image src="/logo.jpg" alt="Logo" width={150} height={100} />
+                </Link>
+                <Navbar />
+            </header>
         </>
     );
 }
